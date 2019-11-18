c5adaf987ef31e9567b50ef0b3f56390de1c74f6
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-11-18 15:37:40 +0700

    040 : Handling Events with Methods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person/Person";
 
@@ -12,22 +11,28 @@ class App extends Component {
     ]
   };
 
+  swtichNameHandler = () => {
+    console.log('Was clicked');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button></button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+        <button onClick={this.swtichNameHandler}>Swtich Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
           My Hobbies: Racing
         </Person>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
       </div>
     );
   }
