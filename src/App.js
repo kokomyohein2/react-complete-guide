095c9cfd8ec827f2a3fb8aadeb590abfaab2d506
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-11-18 15:27:21 +0700

    039 : Props and State

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Steph", age: 26 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
@@ -14,11 +22,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29">
+        <button></button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
           My Hobbies: Racing
         </Person>
-        <Person name="Stephanie" age="26" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
       </div>
     );
   }
