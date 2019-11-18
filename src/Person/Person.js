748ec419081c6258c8b6c6b29308a26d42ce6a70
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-11-18 15:06:15 +0700

    035 : Outputting Dynamic Content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 
 const person = () => {
-    return <p>I am a Person!</p>
+return <p>I am a Person and I am {Math.floor(Math.random() * 30)}!</p>
 }
 
 export default person;
