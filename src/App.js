7e528cd35a054105c13fd1ddfa39447f8abc11ad
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-11-18 16:16:04 +0700

    042 : Manipulating the State

--- src/App.js
+++ src/App.js
@@ -12,8 +12,14 @@ class App extends Component {
   };
 
   swtichNameHandler = () => {
-    console.log('Was clicked');
-  }
+    this.setState({
+      persons: [
+        { name: "ko", age: 1 },
+        { name: "Manu", age: 1 },
+        { name: "Steph", age: 1 }
+      ]
+    });
+  };
 
   render() {
     return (
@@ -24,14 +30,14 @@ class App extends Component {
           age={this.state.persons[0].age}
         />
         <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
         >
           My Hobbies: Racing
         </Person>
         <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
         />
       </div>
     );
