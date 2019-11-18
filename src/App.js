ba6387497413e71681c2111c81d0ffd2e8f113b9
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-11-18 18:05:10 +0700

    044 : Using useState()

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
       { name: "Steph", age: 26 }
-    ]
-  };
+    ],
+    otherState: "some other value"
+  });
 
-  swtichNameHandler = () => {
-    this.setState({
+  const swtichNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: "ko", age: 1 },
         { name: "Manu", age: 1 },
@@ -21,27 +22,25 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.swtichNameHandler}>Swtich Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <button onClick={swtichNameHandler}>Swtich Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
 
-export default App;
+export default app;
