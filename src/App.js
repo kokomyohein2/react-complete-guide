a36882b7e0cabb951ccf26ecc0d5ea121d04c33a
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-11-20 17:44:35 +0700

    057 : Lists and State

--- src/App.js
+++ src/App.js
@@ -32,6 +32,12 @@ class App extends Component {
     });
   };
 
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
   togglePersonHandler = () => {
     const condt = this.state.showPersons;
     this.setState({ showPersons: !condt });
@@ -50,8 +56,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
           })}
         </div>
       );
