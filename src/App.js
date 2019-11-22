c0f09156d3c5a72d53e6b674b509648c4cb7fb86
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-11-22 13:59:16 +0700

    070 : Introducing Styled Components

--- src/App.js
+++ src/App.js
@@ -54,7 +54,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
 
     let persons = null;
@@ -76,6 +80,10 @@ class App extends Component {
       );
 
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black"
+      };
     }
 
     const classes = [];
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,32 @@
 import React from "react";
-import './Person.css';
+import styled from "styled-components";
+
+// import './Person.css';
+
+const StyleDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 2px #ccc;
+  padding: 16px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
 
 const person = props => {
   return (
-    <div className="Person">
+    // <div className="Person">
+    <StyleDiv>
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+      {/* </div> */}
+    </StyleDiv>
   );
 };
 
