b1154004740dd6b5faba293b6b6305d61b2d141b
KoKoMyoHeinHtet <komyo2000@gmail.com>
2020-02-12 07:28:04 +0700

    098 : Optimizing Functional Components with Reach.memo()

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -6,7 +6,10 @@ class Persons extends Component {
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         console.log('[Persons.js] shouldComponentUpdate');
-        return true;
+        if (nextProps.persons !== this.props.persons)
+            return true;
+        else
+            return false;
     }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
